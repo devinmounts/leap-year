7c5d33267117754db388a9105efd9cc71729eaf9
Cynthia Smith and Devin Mounts <devin@>
2018-06-06 09:26:28 -0700

    add final UI to display information in form of a sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,15 @@ $(function(){
     e.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
-  });
 
+    $(".year").text(year);
 
+   if (!result) {                 // same as writing if (result === false)
+     $(".not").text("not");
+   } else {
+     $(".not").text("");
+   }
 
-
-
+   $("#result").show();
+ });
 });
